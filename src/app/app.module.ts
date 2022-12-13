254d75aef9a7f11619f2004f1f646b5228740ae6
jkkoloma <kolomajeremi60@gmail.com>
2022-12-13 17:28:32 +0000

    page inscription ok plus formulaire reactive ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule, // Pour Activer ReactiveFormsModule après l'importation
     AppRoutingModule
   ],
   providers: [],
--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+  submitted = false; // Par défaut le formulaire n'est pas validé;
+
+  inscriptionValidation!: FormGroup // Binding de inscriptionValidation à célui qui se trouve dans le HTML
+
+  // on crée une variable formlBuilder qui sera de type FormBuilder de Angular en mettans les imports à jours, pour le traitement du formulaire. 
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    // Validation, on prend notre variable déclarer pour avoir accès aux formGroupe de FormBuilder
+    this.inscriptionValidation = this.formBuilder.group({
+      // Déclarons les validations qu'on souhaite avoir.
+      // On importe Validators ou mètre à jours les imports, 1ère validation c'est required
+      // Quand l'utilisateur ne mets rien, un message s'affichera que userName est obigatoire.
+      // le userName est à Binder au niveau de input dans le formulaire dans formControlName="userName" avec {ngClase}
+      userName: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  // Quand ton clic sur le boutton S'inscrire
+  onSubmit() {
+    // Maintenant change la valeur de submitted à true;
+    this.submitted = true;
+
+    // vérifions les champs sont invalide
+    if (this.inscriptionValidation.invalid) {
+      return
+    } else {
+      alert("Inscrit avec Succèss !")
+    }
+
+
+
   }
 
 }
