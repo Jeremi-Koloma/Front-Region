fb1c88b0995d070481f7eadcd1b0c1b243240b59
jkkoloma <kolomajeremi60@gmail.com>
2022-12-13 10:32:09 +0000

    ajout de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
