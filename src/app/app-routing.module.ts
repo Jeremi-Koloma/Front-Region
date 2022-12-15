1956bbf752755e49144fd0a5b180145fa8872c0c
jkkoloma <kolomajeremi60@gmail.com>
2022-12-15 19:00:25 +0000

    Gestion des liens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueillComponent } from './accueill/accueill.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { LoginComponent } from './login/login.component';
+import { RegionComponent } from './region/region.component';
 
 const routes: Routes = [
-  {path: "",component: InscriptionComponent}
+  {path: "",component: AccueillComponent},
+  {path: "inscription",component: InscriptionComponent},
+  {path: "login",component: LoginComponent},
+  {path: "accueil",component: AccueillComponent},
+  {path: "regions",component: RegionComponent},
+  
 ];
 
 @NgModule({
